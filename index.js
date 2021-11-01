eb393a73a683933aec0448c9b27e9778bb1702ec
sojjib <developersojib.01@gmail.com>
2021-11-01 20:32:37 +0600

    fifth-commit

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/* const express = require("express");
-const { MongoClient } = require("mongodb");
-const ObjectId = require("mongodb").ObjectId;
-require("dotenv").config();
-// const bodyParser = require('body-parser');
-const cors = require("cors"); */
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -12,22 +6,11 @@ const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 
 
-/* const port = process.env.PORT || 5000;
-// http://localhost:5000
-const app = express();
-// app.use(bodyParser.json());
-
-app.use(cors());
-app.use(express.json()); */
-const port = 5000;
+const port = project.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wcyf6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -39,103 +22,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-
 app.get("/", (req, res) => {
   res.send("Coming soon.....Developer is working on it. Stay connected. For getting");
 });
-
-
-
-
-/* client.connect(err => {
-  const collection = client.db("tourhotel").collection("booking");
-  // perform actions on the collection object
-
-  const tour = [
-    {
-      "key": 1,
-      "img": "https://designexpart.com/tm/html/bahon/assets/img/package/package-1.jpg",
-      "title": "Brazil – All Stunning",
-      "desc": "Where does one begin with Italy? Italy is the land of mouthwatering meals that last all night.",
-      "descL": "Where does one begin with BRAZIL? BRAZIL is the land of mouthwatering meals that last all night, heady wines to drink underneath olive trees, pastel villas built into sheer cliffs above the crystal blue Tyrrhenian Sea, and Renaissance paintings and marble sculptures that leave you awestruck and inspired. It is everyone’s dream to travel to Italy, and for good reason. Whether you are heading to the beach or to the mountains, to the vineyards or to the cities, there are endless possibilities for exciting Italy travel. Check out Travel + Leisure’s Italy travel guide to decide what to eat, where to stay and what to see.",
-      "price": 34.79,
-      "city": "BRAZIL",
-      "tour": "2 DAYS 3 NIGHTS",
-      "rating": 4.2,
-      "ratingCount": "450"
-    },
-    {
-      "key": 2,
-      "img": "https://designexpart.com/tm/html/bahon/assets/img/package/package-2.jpg",
-      "title": "The Statue of Liberty",
-      "desc": " The Statue of Liberty is the land of mouthwatering meals that last all night, heady wines to drink underneath olive trees",
-      "descL": "Whether you are heading to the beach or to the mountains, to the vineyards or to the cities, there are endless possibilities for exciting Italy travel. Check out Travel + Leisure’s Italy travel guide to decide what A good place to start is the capital city of Rome, the birthplace of Western civilization and the site of many epic historical events. All the regions of Italy are different and cater to different kinds of vacations—from skiing in the Dolomites to sunning on the Aeolian Islands..",
-      "price": 422,
-      "city": "NEW YORK",
-      "tour": "3 DAYS 3 NIGHTS",
-      "rating": 4,
-      "ratingCount": "680"
-    },
-    {
-      "key": 3,
-      "img": "https://designexpart.com/tm/html/bahon/assets/img/package/package-3.jpg",
-      "title": "STAND OF THILAND",
-      "desc": "It is everyone’s dream to travel to THILAND, and for good reason.",
-      "descL": "It is everyone’s dream to travel to Italy, and for good reason. Whether you are heading to the beach or to the mountains, to the vineyards or to the cities, there are endless possibilities for exciting Italy travel. Check out Travel + Leisure’s Italy travel guide to decide what to eat, where to stay and what to see.",
-      "price": 495,
-      "city": "ROME",
-      "tour": "1 DAYS 1 NIGHTS",
-      "rating": 210,
-      "ratingCount": "350"
-    },
-    {
-      "key": 4,
-      "img": "https://designexpart.com/tm/html/bahon/assets/img/package/package-4.jpg",
-      "title": "TAJ MAHAL",
-      "desc": "Leisure’s INDIA travel guide to decide what to eat, where to stay and what to see.",
-      "descL": "A good place to start is the capital city of Rome, the birthplace of Western civilization and the site of many epic historical events. All the regions of Italy are different and cater to different kinds of vacations—from skiing in the Dolomites to sunning on the Aeolian Islands.Leisure’s Italy travel guide to decide what to eat, where to stay and what to see.",
-      "price": 48.69,
-      "city": "INDIA",
-      "tour": "5 DAYS 4 NIGHTS",
-      "rating": 750,
-      "ratingCount": "560"
-    },
-    {
-      "key": 5,
-      "img": "https://designexpart.com/tm/html/bahon/assets/img/package/package-5.jpg",
-      "title": "VIEW OF TOKYO",
-      "desc": " Italy is the land of mouthwatering meals that last all night, heady wines to drink underneath olive trees",
-      "descL": " Italy is the land of mouthwatering meals that last all night, heady wines to drink underneath olive trees, pastel villas built into sheer cliffs above the crystal blue Tyrrhenian Sea, and Renaissance paintings and marble sculptures that leave you awestruck and inspired. It is everyone’s dream to travel to Italy,",
-      "price": 469,
-      "city": "TOKYO",
-      "tour": "2 DAYS 3 NIGHTS .18+",
-      "rating": 4,
-      "ratingCount": "500"
-    },
-    {
-      "key": 6,
-      "img": "https://designexpart.com/tm/html/bahon/assets/img/package/package-6.jpg",
-      "title": "SWITZERLAND - COUNTRY OF TRAVEL",
-      "desc": "Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis.",
-      "descL": "Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel",
-      "price": 440,
-      "city": "SWITZERLAND",
-      "tour": "2 DAYS 3 NIGHTS 18+",
-      "rating": 3.5,
-      "ratingCount": "500"
-    }
-  ]
-
-  const result = collection.insertMany(tour);
-  console.log('database send hitted');
-  console.log(result);
-
-  client.close();
-}); */
-
-
-
-
 
 async function run() {
   try {
@@ -143,24 +32,6 @@ async function run() {
     const database = client.db("tourhotel");
     const coursesCollection = database.collection("booking");
     const orderCollection = database.collection("orders");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // load courses get api fir react
     app.get("/booking", async (req, res) => {
