22f8040bf92b08f224be4f333d6136f7debbc15f
sojjib <developersojib.01@gmail.com>
2021-11-01 21:47:45 +0600

    working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 
 
-const port = project.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
